f620279c40442e6b76af4300f5e87ccb9de0ee60
NicKsNiX <76461381+NicKsNiX@users.noreply.github.com>
2024-04-16 00:20:15 +0700

    16/4/2567

--- application/views/main/view_test.php
+++ application/views/main/view_test.php
@@ -3,74 +3,79 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 5 Modal Example</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modal Example</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-    <table id="dataTable">
-        <thead>
-            <tr>
-                <th>v</th>
-                <th>v</th>
-                <th>v</th>
-                <th>v</th>
-                <th>v</th>
-                <th>v</th>
-                <th>v</th>
-            </tr>
-        </thead>
-        <tbody>
+    <form id="myform" class="">
+        <h2 class="form-wizard-heading">BootStap Wizard Form</h2>
+        <input type="text" value="" />
+        <input type="submit" />
+    </form>
 
-        </tbody>
-    </table>
-    <a href="javascript:void(0)" class="btn btn-secondary float-center mdlReceiveDetail" data-bs-toggle="modal" data-bs-target="#mdlEdit"><i class="ti-search"></i> Details</a>
-    <div class="modal fade" id="mdlEdit" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog" role="document">
+    <!-- Modal -->
+    <!-- The Modal -->
+    <div class="modal" id="myModal">
+        <div class="modal-dialog">
             <div class="modal-content">
+                <!-- Modal Header -->
                 <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropEdit">Edit Menu </h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <h4 class="modal-title">Modal Header</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
+                <!-- Modal Body -->
                 <div class="modal-body">
-
+                    Modal body 1
                 </div>
+                <!-- Modal Footer -->
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" id="btnSaveEdit">Save changes</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <div id="target">
+                        Click here
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
+    <div class="modal" id="myModal2">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Modal Header</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <!-- Modal Body -->
+                <div class="modal-body">
+                    Modal body 2
+                </div>
+                <!-- Modal Footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
 
-    <button id="downloadPDF">Download PDF</button>
-    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $('#myform').on('submit', function(ev) {
+            $('#myModal').modal('show');
+            ev.preventDefault();
+        });
+        
+        $("#target").on("click", function() {
+            $('#myModal2').modal('show');
+        });
+        
+    </script>
+
 </body>
 
 </html>
-
-<script src='http://127.0.0.1/IMS_Proplus/assets/js/test.js'></script>
-
-<!-- <script>
-    $(document).ready(function() {
-        $('#downloadPDF').on('click', function() {
-            var doc = new jsPDF();
-
-            // Convert the table to a PDF table
-            doc.autoTable({
-                html: '#dataTable'
-            });
-
-            // Save the PDF
-            doc.save('table.pdf');
-        });
-    });
-</script> -->
--- assets/js/receiveInfo.js
+++ assets/js/receiveInfo.js
@@ -9,7 +9,10 @@ $(document).ready(function() {
         doc.save('table.pdf');
     });
     showYear();
+
     const table = TableReceive();
+    // This function clears the content of all modals
+
     function TableReceive() {
         var year = $('#yearSelect').val();
         var month = $('#monthSelect').val();
@@ -51,14 +54,100 @@ $(document).ready(function() {
         });
     }
     
-    $('#tblStockRecive').on('click', '.mdlReceiveDetail', function () {
+    $('#tblStockRecive').on('click', '.mdlReceiveDetail', function (ev) {
+        $('#detailsModal').modal('show');
+        ev.preventDefault();
         var inv = $(this).data('id');
         // var inv = '12401001';
         // alert(inv)
         showReceiveDetail(inv);
     });
+    // $('#mdlAddReceiveDetail').on('shown.bs.modal', function() {
+    $('#btnAddReceiveDetail').on('click', function() {
+        $('#detailsModal').modal('hide');
+        $('#mdlAddReceiveDetail').modal('show');
+        selProductCode();
+    });
+
+    $('#mdlAddReceiveDetail').on('hidden.bs.modal', function() {
+        $('#detailsModal').modal('show');
+        $('#mdlAddReceiveDetail').modal('hide');
+    });
+    
+    
+    $('#selAddProductCode').on('change',function(){
+        var product_id = $('#selAddProductCode').val();
+        // alert(product_id)
+        $.ajax({
+            url: API_URL + "Receive/getModelById",
+            type: 'POST',
+            dataType: 'json',
+            data:{
+                id:product_id
+            }
+        })
+        .done(function(data) {
+        //    alert(data[0].mpc_model)
+           $('#selAddModel').val(data[0].mpc_model)
+           $('#inpAddDiscription').val(data[0].mpc_discription)
+           $('#inpAddIndex').val(data[0].mib_number)
+           $('#inpAddSize').val(data[0].mib_size)
+           $('#selAddBrand').val(data[0].mb_name)
+           
+        })
+    });
 
     function showReceiveDetail(invoiceNumber) {
+        var apiUrl = 'http://127.0.0.1/api/Receive/getReceiveDetail';
+        $.ajax({
+            url: apiUrl,
+            type: 'GET',
+            dataType: 'json',
+            data: { inv: invoiceNumber },
+            success: function(data) {
+                console.log(data);
+                if (data.length > 0) {
+                    $('#docNumber').val(data[0].isd_doc_number);
+                    $('#docDate').val(data[0].isd_doc_date);
+                    $('#invNumber').val(data[0].isd_inv_no);
+                    $('#poNumber').val(data[0].isd_po_number);
+                    $('#supplierName').val(data[0].isd_customer);
+                    $('#invDate').val(data[0].isd_inv_date);
+                    $('#poDate').val(data[0].isd_po_date);
+                }
+                var html = "";
+                for (var i = 0; i < data.length; i++) {
+                    html += `
+                        <tr>
+                            <td>${i+1}</td>
+                            <td>${data[i].mb_name}</td>
+                            <td>${data[i].mpc_name}</td>
+                            <td>${data[i].mpc_model}</td>
+                            <td>${data[i].mpc_discription}</td>
+                            <td>${data[i].isd_qty}</td>
+                            <td>${data[i].isd_price_unit}</td>
+                            <td>${data[i].isd_qty * data[i].isd_price_unit}</td>
+                            <td>
+                                <a href="#" class="btn btn-secondary mdlReceiveDetail" data-id="${data[i].isd_inv_no}" data-bs-toggle="modal" data-bs-target="#detailsModal"><i class="ti-pencil"></i></a>
+                                <a href="#" class="btn btn-danger mdlOtherButton" data-id="${data[i].isd_inv_no}"><i class="ti-trash"></i></a>
+                            </td>
+                        </tr>`;
+                }
+                $('#tblReceiveDetail tbody').html(html);
+
+                // Initialize DataTables after updating the table content
+                // $('#tblReceiveDetail').DataTable({
+                //     scrollX: true,
+                // });
+            },
+            error: function(xhr, status, error) {
+                console.error(status + ": " + error);
+            }
+        });
+    
+    }
+
+    function insertProductDetail(invoiceNumber) {
         var apiUrl = 'http://127.0.0.1/api/Receive/getReceiveDetail';
         $.ajax({
             url: apiUrl,
@@ -108,6 +197,29 @@ $(document).ready(function() {
     
     }
     
+    function selProductCode(){
+        $.ajax({
+            url: API_URL + "Receive/getSelProductCode",
+            type: 'POST',
+            dataType: 'json',
+        })
+        .done(function(data) {
+            console.log(data); // Use console.log for better debugging
+            // Clear existing table rows
+            $('#selAddProductCode').empty();
+            // Append new options from data received
+            $.each(data, function(index, item) {
+                $('#selAddProductCode').append($('<option>', {
+                    value: item.mpc_id,
+                    text: item.mpc_name
+                }));
+            });
+        })
+        .fail(function(xhr, status, error) {
+            var errorMessage = `${status}: ${error}`;
+            console.log('Error:', errorMessage);
+        });
+    }
 
     function showYear() {
 
@@ -138,26 +250,40 @@ $(document).ready(function() {
 
     }
 
-    $('#btnSaveReceive').on('click', function() {
-        // Serialize form data
-        var formData = new FormData($('#RegisterReceive')[0]);
-        console.log(formData);
-        // Perform AJAX request
+
+    $('#btnSaveAdd').click(function() {
+        var formData = new FormData();
+        formData.append('doc_number', $('#docNumber').val());
+        formData.append('doc_date', $('#docDate').val());
+        formData.append('invoice_number', $('#invNumber').val());
+        formData.append('invoice_date', $('#invDate').val());
+        formData.append('purchase_order', $('#poNumber').val());
+        formData.append('purchase_order_date', $('#poDate').val());
+        formData.append('supplier_name', $('#supplierName').val());
+        formData.append('product_id', $('#selAddProductCode').val());
+        formData.append('qty', $('#inpAddQaulity').val());
+        formData.append('price', $('#inpAddPriceUnit').val());
+        formData.append('discription', $('#inpAddDiscription').val());
+
         $.ajax({
-            url: API_URL + "Receive/getReceiveInfo",
+            url: API_URL + "Receive/insertReceive",
             type: 'POST',
             data: formData,
-            contentType: false,
-            processData: false,
+            processData: false, // Prevent jQuery from automatically processing data
+            contentType: false, // Prevent jQuery from automatically setting contentType
             success: function(response) {
                 // Handle success response
-                alert("success")
-                // Optionally, do something with the response
+                Swal.fire({
+                    title: "Success!",
+                    text: "tum kan add succefully",
+                    icon: "success"
+                  });
+                //   $('#mldAddProduct').modal('hide');
+                initializeDataTable();
             },
             error: function(xhr, status, error) {
                 // Handle error
-                console.log('Error:', error);
-                // Optionally, display error message to the user
+                console.error(xhr.responseText);
             }
         });
     });
@@ -176,4 +302,9 @@ $(document).ready(function() {
             }
         });
     });
+
+    $('#mdlAddReceiveDetail').on('shown.bs.modal', function() {
+        $('#selAddProductCode').trigger('change');
+    });
+
   });
